dc0043a7397985b83950d3dfa1eb024042f65c22
CosMo578 <raphaelakpor00@gmail.com>
2024-10-08 01:04:55 +0100

    Changed image sizing for dynamic route product

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,15 @@ import ProductsListing from "@/components/ProductsListing";
 const HomePage = () => {
   return (
     <>
+    
+      {/* <h2>Categories</h2>
       <ul>
         <li>Clothes</li>
         <li>Electronics</li>
         <li>Furniture</li>
         <li>Shoes</li>
-      </ul>
+        <li>Miscellaneous</li>
+      </ul> */}
       <Header />
       <ProductsListing />
       <Footer />
